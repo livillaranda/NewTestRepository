9a7815bc5a9881ca5b9a2e198a5b2d74447747ee
chaly <chalysama@gmail.com>
2022-06-22 11:59:56 -0400

    Added code to my branch.

---
+++ NewTestRepo/src/main/java/David.java
@@ -0,0 +1,16 @@
+public class David {
+    public static void David(String[] args) {
+
+
+
+    int David= 1;
+            do{
+                System.out.println(David);
+                David= David+1;
+
+            } while(David<1);
+
+
+    }
+
+}
